cf425348b3464673bbe6d7b6121a07904446eada
HassanJkhan99 <Hassanjamilkhan114w@gmail.com>
2022-04-25 09:59:25 +0500

    Chess App

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { Board1Component } from "./board1/board1.component";
+import { HomeComponent } from "./home/home.component";
+import { StartScreenComponent } from "./start-screen/start-screen.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "board1", component: Board1Component },
+  { path: "start", component: StartScreenComponent },
+  { path: "home", component: HomeComponent },
+  { path: "**", component: StartScreenComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { Board1Component } from "./board1/board1.component";
+import { NgxChessBoardModule } from "ngx-chess-board";
+import { HomeComponent } from "./home/home.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { StartScreenComponent } from "./start-screen/start-screen.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Board1Component,
+    HomeComponent,
+    StartScreenComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgxChessBoardModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
